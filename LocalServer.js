391d9ab5e13dfe23f93114b371fc9fc43bfb0630
Gustavo Iha <gustavoiha@gmail.com>
2016-07-31 12:32:42 -0300

    Starting files

---
+++ LocalServer.js
@@ -0,0 +1,53 @@
+var fs = require('fs');
+var http = require('http');
+//var async = require('async');
+
+var txts = ['send1.txt', 'send2.txt', 'send3.txt']
+
+var server = http.createServer(function(request, response) {
+
+  // time
+  var begin = Date.now();
+
+  var reads = [];
+
+  var count = 0
+
+  console.log(request.url);
+
+  for (var i in txts){
+    fs.readFile(txts[i], createReady(i))
+  }
+
+  // createReady(arg) calls this method, but NOT ready. createReady(arg)() calls both!!!!!!!!!!!! ogm, right?
+  function createReady(index){
+    return function ready(error, texto){
+     // Send file content
+
+     if (error){
+       // internal server error
+       response.code = 500;
+       return response.end(error.toString())
+     }
+
+     reads[index] = (texto.toString())
+     count++
+     
+     if (count >= txts.length)
+      response.end(reads.join('\n'))
+
+     var end = Date.now();
+
+     console.log(`file${index}: ${end - begin}`)
+   }
+ }
+
+}).listen(8080); // Activates this server, listening on port 8080.
+
+/*
+obs:
+
+1. headers['user-agent'] = headers.user-agent, but with no ambiguity in the minus sign.
+2. Select text + ctrl d = select next equal
+
+*/
